25927951f41021819943a18e31b4946e67270236
Liam Pugh <liam.pugh.009@gmail.com>
2023-03-11 19:53:50 +0000

    init

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+    .add_plugins(DefaultPlugins)
+    .add_startup_system(setup)
+    .run();
+}
+
+fn setup(
+    mut commands: Commands,
+) {
+    commands.spawn(Camera2dBundle::default());
+}
