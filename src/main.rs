2c9012a3c482dbfd95e9978a622899915abae526
Liam Pugh <liam.pugh.009@gmail.com>
2023-03-12 15:00:14 +0000

    window object working with sprite position

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,12 @@ struct BlockSize {
     h: f32,
 }
 
+#[derive(Resource)]
+pub struct WinSize {
+    w: f32,
+    h: f32,
+}
+
 const SPRITE_SIZE: BlockSize = BlockSize{w:100., h:25.};
 
 fn main() {
@@ -17,7 +23,7 @@ fn main() {
         ..default()
     }))
     .add_startup_system(setup)
-    .add_system(add_player)
+    .add_system(add_player_get_window)
     .run();
 }
 
@@ -27,9 +33,11 @@ fn setup(
     commands.spawn(Camera2dBundle::default());   
 }
 
-fn add_player(mut commands: Commands, window_query: Query<&Window>) {
+fn add_player_get_window(mut commands: Commands, window_query: Query<&Window>) {
     let Some(window) = window_query.get_single().ok() else { return };
     let (win_w, win_h) = (window.width(), window.height());
+
+	commands.insert_resource(WinSize { w: win_w, h: win_h });
     
     commands.spawn(SpriteBundle {
         sprite: Sprite {
@@ -37,7 +45,7 @@ fn add_player(mut commands: Commands, window_query: Query<&Window>) {
             custom_size: Some(Vec2::new(SPRITE_SIZE.w, SPRITE_SIZE.h)),
             ..default()
         },
-        transform: Transform::from_translation(Vec3::new(-50., win_h + SPRITE_SIZE.h, 0.)),
+        transform: Transform::from_translation(Vec3::new(-50., -win_h / 2. + SPRITE_SIZE.h, 0.)),
         ..default()
     });
 }
